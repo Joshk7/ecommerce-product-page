23e36b94bc83c51ea4c2c9d34eba498444765332
JoshK7 <joshk7683@gmail.com>
2025-04-06 17:17:00 -0500

    added logic for checkout dialog

--- script.js
+++ script.js
@@ -26,10 +26,17 @@ const quantityIncrease = document.getElementById("quantity-increase");
 
 const addToCart = document.getElementById("add-to-cart");
 const cartButton = document.getElementById("cart");
+const cartAmount = document.getElementById("cart-amount");
 const checkoutDialog = document.getElementById("checkout-dialog");
 
+const checkoutEmpty = document.getElementById("checkout-empty");
+const checkoutFilled = document.getElementById("checkout-filled");
+
+const checkoutAmount = document.getElementById("checkout-amount");
+const checkoutTotal = document.getElementById("checkout-total");
 
 const mediaQuery = window.matchMedia("(min-width: 64rem)");
+const large = window.matchMedia("(min-width: 40rem)");
 
 const images = [
     "/images/image-product-1.jpg",
@@ -55,7 +62,7 @@ const setMainIndex = (index) => {
     mainThumbnailButtons[index].dataset.selected = "true";
     const newSource = images[mainIndex];
     sliderImage.src = newSource;
-}
+};
 
 const setLightboxIndex = (index) => {
     lightboxThumbnailButtons[lightboxIndex].dataset.selected = "false";
@@ -63,12 +70,12 @@ const setLightboxIndex = (index) => {
     lightboxThumbnailButtons[index].dataset.selected = "true";
     const newSource = images[lightboxIndex];
     lightboxImage.src = newSource;
-}
+};
 
 const setQuantity = (newQuantity) => {
     quantity = newQuantity;
     quantityAmount.textContent = quantity;
-}
+};
 
 const handleOpenMenu = (e) => {
     menuDialog.showModal();
@@ -91,10 +98,11 @@ const handleLeaveMenu = (e) => {
 };
 
 const handleLeaveCheckout = (e) => {
+    console.log(e.target);
     if (!cartButton.contains(e.target) && !checkoutDialog.contains(e.target)) {
         checkoutDialog.close();
     }
-}
+};
 
 const handleOpenLightbox = (e) => {
     lightboxDialog.showModal();
@@ -113,50 +121,75 @@ const handleSliderRight = (e) => {
 const handleLightboxLeft = (e) => {
     const newIndex = (lightboxIndex - 1 + 4) % 4;
     setLightboxIndex(newIndex);
-}
+};
 
 const handleLightboxRight = (e) => {
     const newIndex = (lightboxIndex + 1) % 4;
     setLightboxIndex(newIndex);
-}
+};
 
 const handleMainThumbnail = (e) => {
     const newIndex = e.target.dataset.index;
     setMainIndex(newIndex);
-}
+};
 
 const handleCloseLightbox = (e) => {
     lightboxDialog.close();
-}
+};
 
 const handleLightboxThumbnail = (e) => {
     const newIndex = e.target.dataset.index;
     setLightboxIndex(newIndex);
-}
+};
 
 const handleQuantityDecrease = (e) => {
     if (quantity > 0) {
         setQuantity(quantity - 1);
     }
-}
+};
 
 const handleQuantityIncrease = (e) => {
     setQuantity(quantity + 1);
-}
+};
 
 const handleCartButton = (e) => {
+    if (large.matches) {
+        const rect = cartButton.getBoundingClientRect();
+        checkoutDialog.style.position = "absolute";
+        checkoutDialog.style.top = `${rect.bottom + window.scrollY}px`;
+        checkoutDialog.style.left = `${
+            rect.left + rect.width / 2 + window.scrollX
+        }px`;
+        checkoutDialog.style.transform = "translateX(-50%)";
+    }
+
     if (checkoutDialog.open) {
         checkoutDialog.close();
     } else {
         checkoutDialog.show();
     }
-}
+};
 
 const handleLargeScreen = (e) => {
     if (e.matches) {
         lightboxButton.disabled = false;
     }
-}
+};
+
+const handleAddToCart = (e) => {
+    const productQuantity = parseInt(quantityAmount.textContent);
+    if (productQuantity === 0) {
+        cartAmount.textContent = "";
+        checkoutFilled.style.display = "none";
+        checkoutEmpty.style.display = "block";
+    } else {
+        checkoutAmount.textContent = productQuantity;
+        checkoutTotal.textContent = `$${productQuantity * 125}`;
+        cartAmount.textContent = productQuantity;
+        checkoutFilled.style.display = "flex";
+        checkoutEmpty.style.display = "none";
+    }
+};
 
 menuButton.addEventListener("click", handleOpenMenu);
 // menuDialog.addEventListener("click", handleLeaveMenu);
@@ -177,5 +210,7 @@ quantityDecrease.addEventListener("click", handleQuantityDecrease);
 quantityIncrease.addEventListener("click", handleQuantityIncrease);
 cartButton.addEventListener("click", handleCartButton);
 handleLargeScreen(mediaQuery);
+
+addToCart.addEventListener("click", handleAddToCart);
 
 document.addEventListener("click", handleLeaveCheckout);
